8b0b849d95147b879789a5cd85833abd43db634f
Tom24my <219g0053@s.obirin.ac.jp>
2025-12-04 22:23:41 +0900

    prettier, eslintの設定を追加

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// .prettierrc.js
+module.exports = {
+    // 文字列はシングルクォートを使う (e.g. 'abc')
+    singleQuote: true,
+    // 末尾のカンマはES5準拠で省略
+    trailingComma: 'es5',
+    // セミコロンを省略する
+    semi: false,
+    // インデントはスペース2
+    tabWidth: 2,
+    // 常に括弧で囲む (例: (a) => a)
+    arrowParens: 'always',
+    // 1行の文字数
+    printWidth: 100,
+}
